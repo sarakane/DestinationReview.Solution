ba3a62e2f4e626adc8adbc9035f0370c10704fdd
JohnNils Olson <johnnils@gmail.com>
2020-08-17 13:08:06 -0700

    add migration for destinations
    Co-Authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ DestinationReview/Migrations/20200817200536_Destination.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DestinationReview.Migrations
+{
+    public partial class Destination : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Reviews",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Reviews",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DestinationId",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Destinations",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Country = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinations", x => x.DestinationId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_DestinationId",
+                table: "Reviews",
+                column: "DestinationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Destinations_DestinationId",
+                table: "Reviews",
+                column: "DestinationId",
+                principalTable: "Destinations",
+                principalColumn: "DestinationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Destinations_DestinationId",
+                table: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Destinations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_DestinationId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "DestinationId",
+                table: "Reviews");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
