1e13e154981872d282912a89b53e9fc59a3bc046
JohnNils Olson <johnnils@gmail.com>
2020-08-17 11:41:25 -0700

    add data annotations
    Co-Authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- DestinationReview/Models/Review.cs
+++ DestinationReview/Models/Review.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DestinationReview.Models
 {
   public class Review
   {
     public int ReviewId { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Title { get; set; }
+    [Required]
     public string Body { get; set; }
+    [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
     public int Rating { get; set; }
   }
 }
