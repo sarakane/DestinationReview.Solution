b49f8bb700e89633b071d53499c20031307a96c0
JohnNils Olson <johnnils@gmail.com>
2020-08-17 11:15:34 -0700

    add reviewscontroller
    Co-Authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ DestinationReview/Controllers/ReviewsController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using DestinationReview.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DestinationReview.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ReviewsController : ControllerBase
+  {
+    private DestinationReviewContext _db;
+    public ReviewsController(DestinationReviewContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Review>> Get()
+    {
+      return _db.Reviews.ToList();
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Review review)
+    {
+      _db.Reviews.Add(review);
+      _db.SaveChanges();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Review> Get(int id)
+    {
+      return _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Review review)
+    {
+      review.ReviewId = id;
+      _db.Entry(review).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var reviewToDelete = _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
+      _db.Reviews.Remove(reviewToDelete);
+      _db.SaveChanges();
+    }
+  }
+}
